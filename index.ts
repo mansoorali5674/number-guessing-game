6f4178a63865d200dd23ef0cc47de7e4809ebb9e
Mansoor Ahmed <160484170+mansoorali5674@users.noreply.github.com>
2024-03-31 05:54:04 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,38 @@
+console.log(" guess a number between 1 to 15")
+
+//creating variables
+let a:number=Math.floor(Math.random()*15)
+import inquirer from "inquirer"
+
+while(true){
+   let input=await inquirer.prompt
+   ({name:"guess", type:"number",
+     message:"enter your guess number"})
+ 
+ //code for given number
+   let ans:number=input.guess
+
+if(a==ans)
+{console.log("Congratulation your guess number is correct")
+break;}
+else{console.log("Sorry try again")}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
